045e3276b7a60d2b43b8623370b41ec7730225c3
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-07-25 14:31:04 +0800

    1

--- tensorboard.sh
+++ tensorboard.sh
@@ -4,6 +4,7 @@ if [ -n "$1" ]; then
    nohup tensorboard --port 10086 --logdir=logs/train_"$1" > logs/tensorboard.out 2>&1 &
 else
    echo "Usage: tensorboard.sh [mode], mode can be cnn/rnn"
+fi
 
 
 
