59c57bcb29fb8f87d2a1eefdeeaaa4f0a7e209e3
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-07-25 14:41:20 +0800

    modify

--- start_train.sh
+++ start_train.sh
@@ -3,8 +3,12 @@
 # multiple command in one line
 
 if [ -f ".trainpid" ]; then
-  echo found running instance `cat .trainpid`
+  if [ -d /proc/`cat .trainpid` ]; then
+     echo found running pid `cat .trainpid`
+  else
+     echo [remove dead pid `cat .trainpid`] && rm .trainpid
+     echo [start training ...] && nohup python3 face_landmark.py -mode=train >logs/train.out 2>&1 & echo $! > .trainpid && tail -f logs/train.out
+  fi
 else
-  echo [start training ...]
-  nohup python3 face_landmark.py -mode=train >logs/train.out 2>&1 & echo $! > .trainpid && tail -f logs/train.out
+  echo [start training ...] && nohup python3 face_landmark.py -mode=train >logs/train.out 2>&1 & echo $! > .trainpid && tail -f logs/train.out
 fi
