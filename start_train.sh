ed851c0fdcf145e877c450f2b7193979eb1b55db
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-07-26 11:36:39 +0800

    modify

--- start_train.sh
+++ start_train.sh
@@ -2,13 +2,22 @@
 
 # multiple command in one line
 
-if [ -f ".trainpid" ]; then
-  if [ -d /proc/`cat .trainpid` ]; then
-     echo found running pid `cat .trainpid`
-  else
-     echo [remove dead pid `cat .trainpid`] && rm .trainpid
-     echo [start training ...] && nohup python3 face_landmark.py -mode=train >logs/train.out 2>&1 & echo $! > .trainpid && tail -f logs/train.out
-  fi
+if [ -n "$1" ]; then
+    if [ -f ".trainpid" ]; then
+        if [ -d /proc/`cat .trainpid` ]; then
+            echo found running pid `cat .trainpid`
+        else
+            rm .trainpid \
+            && echo [remove dead pid `cat .trainpid`] \
+            && nohup python3 face_landmark.py -mode=train -network="$1">logs/train_"$1".out 2>&1 & echo $! > .trainpid \
+            && echo [train "$1" started]
+        fi
+    else
+        nohup python3 face_landmark.py -mode=train -network="$1" >logs/train_"$1".out 2>&1 & echo $! > .trainpid \
+        && echo [train "$1" started]
+    fi
 else
-  echo [start training ...] && nohup python3 face_landmark.py -mode=train >logs/train.out 2>&1 & echo $! > .trainpid && tail -f logs/train.out
+    echo "Usage: start_train.sh [mode], mode can be cnn/rnn"
 fi
+
+
