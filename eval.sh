ca27e979e650cf8b69b05002b3cb6cf63ae008dd
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-07-26 11:44:31 +0800

    modify

--- eval.sh
+++ eval.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-python3 face_landmark.py -cuda_visible_devices=2 -mode=eval
+if [ -n "$1" ]; then
+    python3 face_landmark.py -cuda_visible_devices=2 -mode=eval -network="$1"
+else
+    echo "Usage: start_train.sh [mode], mode can be cnn/rnn"
+fi
