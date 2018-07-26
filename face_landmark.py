387dd345775342124e44bbec797211930a81928a
lotus.jzx <lotus.jzx@alibaba-inc.com>
2018-07-26 21:58:59 +0800

    modified

--- face_landmark.py
+++ face_landmark.py
@@ -50,54 +50,14 @@ exported_model_dir=FLAGS.exported_model_dir+"_"+FLAGS.network
 
 
 def rnn(features,mode):
-    inputs = tf.to_float(features['x'], name="input_to_float")
 
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        is_training=True
-    elif mode == tf.estimator.ModeKeys.EVAL or mode == tf.estimator.ModeKeys.PREDICT:
-        is_training = False
-    else:
-        raise ValueError('unknown mode %s'%mode)
+    resnet_image_size=224
 
-    net, _= resnet_v2.resnet_v2_101(inputs,num_classes=None,is_training=is_training,global_pool=True,reuse=tf.AUTO_REUSE)
-
-    # # output layer. Between 0 and 1 (sigmoid)
-    # net = layers_lib.conv2d(
-    #     net,
-    #     136, [1, 1],
-    #     activation_fn=tf.nn.sigmoid,
-    #     normalizer_fn=None,
-    #     scope='fc')
-    #
-    # shape=net.get_shape()
-    # net=tf.reshape(net,[-1,shape[1]*shape[2]*shape[3]],name='logits')
-
-    # Flatten tensor into a batch of vectors
-    flatten = tf.layers.flatten(inputs=net)
-
-    # Dense layer 1, a fully connected layer.
-    dense1 = tf.layers.dense(
-        inputs=flatten,
-        units=1024,
-        activation=tf.nn.relu,
-        use_bias=True)
-
-    # Dense layer 2, also known as the output layer.Between 0 and 1 (sigmoid)
-    logits = tf.layers.dense(
-        inputs=dense1,
-        units=136,
-        activation=tf.nn.sigmoid,
-        use_bias=True,
-        name="logits")
-
-    return logits
-
-def cnn(features,mode):
     """
-    cnn Implementation with slim
-    :param features:
-    :param mode:
-    :return:
+        rnn Implementation with slim
+        :param features:
+        :param mode:
+        :return:
     """
 
     # |== Layer 0: input layer ==|
@@ -107,42 +67,28 @@ def cnn(features,mode):
     assert features['x'].shape[1:] == (
         IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL), "Image size does not match."
 
-    inputs = tf.to_float(features['x'], name="input_to_float")
+    images = tf.to_float(features['x'], name="input_to_float")
 
-    with slim.arg_scope([slim.conv2d],kernel_size=[3,3],stride=1,padding='SAME',activation_fn=tf.nn.relu), \
-            slim.arg_scope([slim.max_pool2d],kernel_size=[2,2],padding='SAME'):
+    inputs = tf.image.resize_images(images, [resnet_image_size, resnet_image_size])
 
-        with tf.variable_scope("layer1"):
-            net = slim.conv2d(inputs,num_outputs=32)
-            net = slim.max_pool2d(net,stride =2)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        is_training=True
+    elif mode == tf.estimator.ModeKeys.EVAL or mode == tf.estimator.ModeKeys.PREDICT:
+        is_training = False
+    else:
+        raise ValueError('unknown mode %s'%mode)
 
-        with tf.variable_scope("layer2"):
-            net = slim.conv2d(net,num_outputs=64)
-            net = slim.conv2d(net, num_outputs=64)
-            net = slim.max_pool2d(net,stride =2)
+    net, _= resnet_v2.resnet_v2_50(inputs,num_classes=None,is_training=is_training,global_pool=False,reuse=tf.AUTO_REUSE)
 
-        with tf.variable_scope("layer3"):
-            net = slim.conv2d(net, num_outputs=64)
-            net = slim.conv2d(net, num_outputs=64)
-            net = slim.max_pool2d(net,stride =2)
+    with tf.variable_scope("fc"):
+        net = slim.flatten(net)
+        net = slim.fully_connected(net, num_outputs=1024, activation_fn=tf.nn.relu)
+        logits = slim.fully_connected(net, num_outputs=136, activation_fn=tf.nn.sigmoid)
 
-        with tf.variable_scope("layer4"):
-            net = slim.conv2d(net, num_outputs=128)
-            net = slim.conv2d(net, num_outputs=128)
-            net = slim.max_pool2d(net,stride=2)
-
-        with tf.variable_scope("layer5"):
-            net = slim.conv2d(net, num_outputs=256)
-
-        with tf.variable_scope("layer6"):
-            net =slim.flatten(net)
-            net =slim.fully_connected(net,num_outputs=1024,activation_fn=tf.nn.relu)
-            net = slim.fully_connected(net, num_outputs=136, activation_fn=tf.nn.sigmoid)
-
-        return net
+    return logits
 
 
-def cnn2(features,mode):
+def cnn(features,mode):
     """
     cnn Implementation with slim
     :param features:
@@ -179,7 +125,7 @@ def cnn2(features,mode):
         with tf.variable_scope("layer4"):
             net = slim.conv2d(net, num_outputs=128)
             net = slim.conv2d(net, num_outputs=128)
-            net = slim.max_pool2d(net,stride=1)
+            net = slim.max_pool2d(net,stride=2)
 
         with tf.variable_scope("layer5"):
             net = slim.conv2d(net, num_outputs=256)
@@ -190,181 +136,6 @@ def cnn2(features,mode):
             net = slim.fully_connected(net, num_outputs=136, activation_fn=tf.nn.sigmoid)
 
         return net
-
-
-def cnn3(features,mode):
-    layers=[]
-
-    """
-        The model function for the network.
-        """
-    # |== Layer 0: input layer ==|
-    # Input feature x should be of shape (batch_size, image_width, image_height, color_channels).
-    # Image shape should be checked for safety reasons at early stages, and could be removed
-    # before training actually starts.
-    # assert features['x'].shape[1:] == (
-    #     IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL), "Image size does not match."
-    inputs = tf.to_float(features['x'], name="input_to_float")
-    layers.append(inputs)
-
-    # |== Layer 1 ==|
-
-    # Convolutional layer.
-    # Computes 32 features using a 3x3 filter with ReLU activation.
-    conv1 = tf.layers.conv2d(
-        inputs=inputs,
-        filters=32,
-        kernel_size=[3, 3],
-        strides=(1, 1),
-        padding='valid',
-        activation=tf.nn.relu)
-    layers.append(conv1)
-
-    # Pooling layer.
-    # First max pooling layer with a 2x2 filter and stride of 2.
-    pool1 = tf.layers.max_pooling2d(
-        inputs=conv1,
-        pool_size=[2, 2],
-        strides=(2, 2),
-        padding='valid')
-    layers.append(pool1)
-
-    # |== Layer 2 ==|
-
-    # Convolutional layer
-    # Computes 64 features using a 3x3 filter with ReLU activation.
-    conv2 = tf.layers.conv2d(
-        inputs=pool1,
-        filters=64,
-        kernel_size=[3, 3],
-        strides=(1, 1),
-        padding='valid',
-        activation=tf.nn.relu)
-    layers.append(conv2)
-
-    # Convolutional layer
-    # Computes 64 features using a 3x3 filter with ReLU activation.
-    conv3 = tf.layers.conv2d(
-        inputs=conv2,
-        filters=64,
-        kernel_size=[3, 3],
-        strides=(1, 1),
-        padding='valid',
-        activation=tf.nn.relu)
-    layers.append(conv3)
-
-    # Pooling layer
-    # Second max pooling layer with a 2x2 filter and stride of 2.
-    pool2 = tf.layers.max_pooling2d(
-        inputs=conv3,
-        pool_size=[2, 2],
-        strides=(2, 2),
-        padding='valid')
-    layers.append(pool2)
-
-    # |== Layer 3 ==|
-
-    # Convolutional layer
-    # Computes 64 features using a 3x3 filter with ReLU activation.
-    conv4 = tf.layers.conv2d(
-        inputs=pool2,
-        filters=64,
-        kernel_size=[3, 3],
-        strides=(1, 1),
-        padding='valid',
-        activation=tf.nn.relu)
-    layers.append(conv4)
-
-    # Convolutional layer
-    # Computes 64 features using a 3x3 filter with ReLU activation.
-    conv5 = tf.layers.conv2d(
-        inputs=conv4,
-        filters=64,
-        kernel_size=[3, 3],
-        strides=(1, 1),
-        padding='valid',
-        activation=tf.nn.relu)
-    layers.append(conv5)
-
-    # Pooling layer
-    # Third max pooling layer with a 2x2 filter and stride of 2.
-    pool3 = tf.layers.max_pooling2d(
-        inputs=conv5,
-        pool_size=[2, 2],
-        strides=(2, 2),
-        padding='valid')
-    layers.append(pool3)
-
-    # |== Layer 4 ==|
-
-    # Convolutional layer
-    # Computes 128 features using a 3x3 filter with ReLU activation.
-    conv6 = tf.layers.conv2d(
-        inputs=pool3,
-        filters=128,
-        kernel_size=[3, 3],
-        strides=(1, 1),
-        padding='valid',
-        activation=tf.nn.relu)
-    layers.append(conv6)
-
-    # Convolutional layer
-    # Conputes 128 features using a 3x3 filter with ReLU activation.
-    conv7 = tf.layers.conv2d(
-        inputs=conv6,
-        filters=128,
-        kernel_size=[3, 3],
-        strides=(1, 1),
-        padding='valid',
-        activation=tf.nn.relu)
-    layers.append(conv7)
-
-    # Pooling layer
-    # Fourth max pooling layer with a 2x2 filter and stride of 2.
-    pool4 = tf.layers.max_pooling2d(
-        inputs=conv7,
-        pool_size=[2, 2],
-        strides=(1, 1),
-        padding='valid')
-    layers.append(pool4)
-
-    # |== Layer 5 ==|
-
-    # Convolutional layer
-    conv8 = tf.layers.conv2d(
-        inputs=pool4,
-        filters=256,
-        kernel_size=[3, 3],
-        strides=(1, 1),
-        padding='valid',
-        activation=tf.nn.relu)
-    layers.append(conv8)
-
-    # |== Layer 6 ==|
-
-    # Flatten tensor into a batch of vectors
-    flatten = tf.layers.flatten(inputs=conv8)
-    layers.append(layers)
-
-    # Dense layer 1, a fully connected layer.
-    dense1 = tf.layers.dense(
-        inputs=flatten,
-        units=1024,
-        activation=tf.nn.relu,
-        use_bias=True)
-    layers.append(dense1)
-
-    # Dense layer 2, also known as the output layer.Between 0 and 1 (sigmoid)
-    logits = tf.layers.dense(
-        inputs=dense1,
-        units=136,
-        activation=tf.nn.sigmoid,
-        use_bias=True,
-        name="logits")
-    layers.append(logits)
-
-    # return layers
-    return logits
 
 def get_optimizer():
     learning_rate=FLAGS.learning_rate
